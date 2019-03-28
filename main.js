4f7daa8fc3796c83e141b847c1131d5155323c9f
PatouBC <48127990+PatouBC@users.noreply.github.com>
2019-03-28 18:41:52 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,57 +1,34 @@
-
-var bouton = document.getElementById("start");
-
-var lgreen = './images/l_green.png';
-var lblue = './images/l_blue.png';
-var lred = './images/l_red.png';
-var lyellow = './images/l_yellow.png';
-
-var light = [lgreen, lblue, lred, lyellow];
-
-var green = './images/green.png';
-var blue = './images/blue.png';
-var red = './images/red.png';
-var yellow = './images/yellow.png';
-
-var imgRed = document.getElementById("red");
-var imgBlue = document.getElementById("blue");
-var imgYellow = document.getElementById("yellow");
-var imgGreen = document.getElementById("green");
-
-var choixCPU = light[ Math.floor(Math.random()*light.length)];
-
-
-function seqOrdi(){
-	if (choixCPU == lgreen){
-		imgGreen.setAttribute("src", choixCPU);
-	}else if (choixCPU == lblue){
-		imgBlue.setAttribute("src", choixCPU);
-	}else if (choixCPU == lred){
-		imgRed.setAttribute("src", choixCPU);
-	}else {
-		imgYellow.setAttribute("src", choixCPU);
+class Button{
+	constructor(element, color, lightColor, soundPath){
+		this.el = element;
+		this.lcolor = lightColor;
+		this.color = color;
+		this.soundPath = soundPath;
+		this.launchListeners();
 	}
-	setTimeout(function(){
-		if (choixCPU == lgreen){
-			imgGreen.setAttribute("src", green);
-		}else if (choixCPU == lblue){
-			imgBlue.setAttribute("src", blue);
-		}else if (choixCPU == lred){
-			imgRed.setAttribute("src", red);
-		}else {
-			imgYellow.setAttribute("src", yellow);
+	play(){
+		var s = new Audio(this.soundPath);
+		s.play();
+	}
+	changeImg(){
+		this.el.setAttribute('src', this.lcolor );
+
+	}
+	timer(){
+			this.el.setAttribute('src', this.color);
+	}
+
+	launchListeners(){
+		var me = this;
+		this.el.onclick = function (){
+			me.play();
+			me.changeImg();
+			setTimeout(function(){
+				me.timer();
+			},500);
 		}
-	},2000);
+	}
 }
 
-function jouer(){
-	seqOrdi();
 
-}
-
-bouton.addEventListener("click", function start(){
-	bouton.setAttribute("style","background-color: red;");
-	bouton.removeEventListener("click", start);
-	jouer();
-});
 
